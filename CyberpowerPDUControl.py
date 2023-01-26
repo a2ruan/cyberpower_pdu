0633058b11bf17c22c6203d240e7557af7efdab9
iqminiclip <30330337+a2ruan@users.noreply.github.com>
2023-01-26 02:36:21 -0500

    Cleaning up comments/function descriptions

--- CyberpowerPDUControl.py
+++ CyberpowerPDUControl.py
@@ -3,7 +3,7 @@ import time
 
 class CyberpowerPDUControl:
     def __init__(self):
-        #command = input("Enter status:")
+        # Constructor
         self.username = "cyber"
         self.password = "cyber"
         self.host = "10.3.230.88" # Example PDU IP only
@@ -11,11 +11,14 @@ class CyberpowerPDUControl:
         self.command = "on"
         
     def set_socket(self, state, socket_number):
+        # Set socket state
+        # state (string) is either "on" or "off"
+        # socket_number is an integer between 1~8 corresponding to the socket of the PDU
+        
         self.socket_number = str(socket_number)
         self.command = str(state)
         asyncio.set_event_loop(asyncio.SelectorEventLoop()) # Workaround for connection error in telnetlib3
         loop = asyncio.get_event_loop() 
-        #loop = asyncio.new_event_loop()
         coro = telnetlib3.open_connection(self.host, 23, shell=self.shell)
         print("Running telnet loop until complete")
         reader, writer = loop.run_until_complete(coro)
@@ -24,6 +27,7 @@ class CyberpowerPDUControl:
         print("Finished asyncio loop")
 
     async def shell(self, reader, writer):
+        # Async function used to telnet into the PDU controller
         initial_time = time.time()
         while True:
             if (time.time() - initial_time) > 30:
@@ -52,14 +56,13 @@ class CyberpowerPDUControl:
                 writer.write(f"\nexit\n\r")
                 break
 
-            # display all server output
-            #print(outp, flush=True)
-
-        # EOF
         print("Exiting telnet")
 
 if __name__ == "__main__":
+    print("Unit Testing")
+    
     command = input("Enter status:")
     pdu_control = CyberpowerPDUControl()
     pdu_control.set_socket(command,1)    
+    
     print("Done!")
